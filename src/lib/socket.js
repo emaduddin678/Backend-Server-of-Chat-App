f2ebe12111c8d688c692dae24ce43ff7b09c9162
emaduddin678 <emaduddin678.com>
2025-01-02 22:50:13 +0600

    online user emit response done

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -9,17 +9,24 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [frontendUrl],
-    // credentials: true,
   },
 });
 
+const userSocketMap = {};
+
 io.on("connection", (socket) => {
   console.log("An user connected", socket.id);
-  
-  socket.on("disconnect",()=>{
-      console.log("An user disconnected", socket.id);
 
-  })
+  const userId = socket.handshake.query.userId;
+  
+  if (userId) userSocketMap[userId] = socket.id;
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("An user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
 });
 
 export { io, app, server };
